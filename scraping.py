709d50fcef68c6edd912f0131421eb5d096d51e8
rwbxd <31582073+rwbxd@users.noreply.github.com>
2020-09-12 06:08:36 -0500

    Optimized a small bit
    -Instead of adding kills to a list and summing the list afterwards, just add the kills value to the existing kills value
    -Moved the "page" assignment into the try statment

--- scraping.py
+++ scraping.py
@@ -6,47 +6,43 @@ def KDAFinder():
 	player = input("Player?\n")
 	url = f"https://liquipedia.net/valorant/{player}/Matches"
 	try:
-		urllib.request.urlopen(url)
+		page = urllib.request.urlopen(url)
 	except:
 		print("not found!\n")
 		KDAFinder()
-	page = urllib.request.urlopen(url)
 	soup = BeautifulSoup(page, "lxml")
 	all_tables = soup.find_all("table")
 	right_table=soup.find("table")
 
-	K=[]
-	D=[]
-	A=[]
-	R=[]
+	K = 0
+	D = 0
+	A = 0
+	R = 0
 	matches = 0
 
 	for row in right_table.findAll('tr')[1:]:
 		matches += 1
 		cells=row.findAll('td')
 		try:
-			K.append(float(cells[7].find(text=True)))
+			K += float(cells[7].find(text=True))
 		except:
 			print("A match has missing values!")
 		try:
-			D.append(float(cells[8].find(text=True)))
+			D += float(cells[8].find(text=True))
 		except:
 			pass
 		try:
-			A.append(float(cells[9].find(text=True)))
+			A += float(cells[9].find(text=True))
 		except:
 			pass
-	kills = sum(K)
-	deaths = sum(D)
-	assists = sum(A)
 
-	kda = Decimal((((kills) + (assists))/deaths))
+	kda = Decimal((K + A)/D)
 	roundedkda = round(kda, 2)
 
 	print()
-	print(f"Kills: {kills}")
-	print(f"Deaths: {deaths}")
-	print(f"Assists: {assists}")
+	print(f"Kills: {K}")
+	print(f"Deaths: {D}")
+	print(f"Assists: {A}")
 	print(f"Matches scraped: {matches}")
 	print(f"KDA Ratio: {roundedkda}")
 	print()
